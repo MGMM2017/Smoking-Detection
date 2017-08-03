d7ffdbdb52286315be23f22a3ff2aa84c390e819
Deniz Sengun <denizsengun89@gmail.com>
2017-08-03 14:02:45 +0200

    Merge branch 'location' of https://github.com/MGMM2017/Smoking-Detection into finalfirst
    # Conflicts:
    #	DenizApp/app/src/main/java/com/example/rigby/denizapp/MainActivity.java

--- DenizApp/app/src/main/java/com/example/rigby/denizapp/graphs.java
+++ DenizApp/app/src/main/java/com/example/rigby/denizapp/graphs.java
@@ -5,24 +5,24 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
+
 import static com.example.rigby.denizapp.R.id.graph;
 
     public class graphs extends AppCompatActivity {
-        Button button;
-        TextView textView;
+        TextView textView2;
         List<TimeAndLocation> values;
         private TimeAndLocationDataSource timeAndLocationSource;
         private Spinner spinner;
@@ -52,6 +52,7 @@ import static com.example.rigby.denizapp.R.id.graph;
             //else{
                 //Integer a = values.size();
                 //textView.setText(a.toString());
+            themostcommonsmokingtimes();
                 drawGraphHourly(context);
 
             //}
@@ -376,6 +377,210 @@ import static com.example.rigby.denizapp.R.id.graph;
             builder.setTitle(title);
             builder.setMessage(message);
             builder.show();
+        }
+
+        private int[] themostcommonsmokingtimes(){
+            int numberOfLabels = values.size();
+            GraphView graphView = (GraphView) findViewById(graph);
+            graphView.removeAllSeries();
+
+
+            // generate Dates
+            java.util.Date dates[] = new java.util.Date[numberOfLabels];
+            int eightTen = 0;
+            int tenTwelve = 0;
+            int twelveTwo = 0;
+            int twoFour = 0;
+            int fourSix = 0;
+            int sixEight = 0;
+            int eigtTen = 0;
+            int tenTwelven = 0;
+            int twelvetwoN = 0;
+            int twoFuorN = 0;
+            int fourSixN = 0;
+            int sixEightN = 0;
+
+
+            int maxnumbertwo = 0;
+            int maxhour = 0;
+
+
+
+            int mon =0;
+            int tue =0;
+            int wed =0;
+            int thu =0;
+            int fri =0;
+            int sat =0;
+            int sun =0;
+            int maxnumber=0;
+            int maxday=0;
+
+            SimpleDateFormat sdfmt1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            for (int a = 0; a < values.size(); a++) {
+
+                try {
+                    dates[a] = sdfmt1.parse(values.get(a).getTime());
+                    if (dates[a].getHours() == 0 || dates[a].getHours() == 1) {
+                        twelvetwoN++;
+                    } else if (dates[a].getHours() == 2 || dates[a].getHours() == 3) {
+                        twoFuorN++;
+                    } else if (dates[a].getHours() == 4 || dates[a].getHours() == 5) {
+                        fourSixN++;
+                    } else if (dates[a].getHours() == 6 || dates[a].getHours() == 7) {
+                        sixEightN++;
+                    } else if (dates[a].getHours() == 8 || dates[a].getHours() == 9) {
+                        eightTen++;
+                    } else if (dates[a].getHours() == 10 || dates[a].getHours() == 11) {
+                        tenTwelve++;
+                    } else if (dates[a].getHours() == 12 || dates[a].getHours() == 13) {
+                        twelveTwo++;
+                    } else if (dates[a].getHours() == 14 || dates[a].getHours() == 15) {
+                        twoFour++;
+                    } else if (dates[a].getHours() == 16 || dates[a].getHours() == 17) {
+                        fourSix++;
+                    } else if (dates[a].getHours() == 18 || dates[a].getHours() == 19) {
+                        sixEight++;
+                    } else if (dates[a].getHours() == 20 || dates[a].getHours() == 21) {
+                        eigtTen++;
+                    } else if (dates[a].getHours() == 22 || dates[a].getHours() == 23) {
+                        tenTwelven++;
+                    }
+                    //System.out.println("DenizDBG getmonth =" + dates[a].getHours());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            maxnumbertwo = Math.max(twelvetwoN,Math.max(twoFuorN,Math.max(fourSixN,Math.max(sixEightN,Math.max(eightTen,Math.max(tenTwelve,Math.max(twelveTwo,
+                    Math.max(sixEight,Math.max(fourSix,Math.max(eigtTen,Math.max(tenTwelven,twoFour)))))))))));
+            String mcHour = " ";
+            if (maxnumbertwo == twelvetwoN) {
+                mcHour = "00:00 - 02:00";
+                maxhour = 0;
+            }
+            else if(maxnumbertwo == twoFuorN){
+                mcHour = "02:00 - 04:00";
+                maxhour =2;}
+            else if(maxnumbertwo == fourSixN){
+                mcHour = "04:00 - 06:00";
+
+                maxhour =4;}
+            else if(maxnumbertwo == sixEightN){
+                mcHour = "06:00 - 08:00";
+
+                maxhour =6;}
+            else if(maxnumbertwo == eightTen){
+                mcHour = "08:00 - 10:00";
+
+                maxhour =8;}
+            else if(maxnumbertwo == tenTwelve){
+                mcHour = "10:00 - 12:00";
+
+                maxhour =10;}
+            else if(maxnumbertwo == twelveTwo){
+                mcHour = "12:00 - 14:00";
+
+                maxhour =12;}
+            else if(maxnumbertwo == twoFour){
+                mcHour = "14:00 - 16:00";
+
+                maxhour =14;}
+            else if(maxnumbertwo == fourSix){
+                mcHour = "16:00 - 18:00";
+
+                maxhour =16;}
+            else if(maxnumbertwo == sixEight){
+                mcHour = "18:00 - 20:00";
+
+                maxhour =18;}
+            else if(maxnumbertwo == eigtTen){
+                mcHour = "20:00 - 22:00";
+
+                maxhour =20;}
+            else if(maxnumbertwo == tenTwelven){
+                mcHour = "22:00 - 24:00";
+
+                maxhour =22;}
+
+
+
+
+
+            for (int b = 0; b<values.size();b++){
+                    try {
+                        dates[b] = sdfmt1.parse(values.get(b).getTime());
+                        if(dates[b].getDay() == 0){
+                            sun++;
+                        }
+                        else if (dates[b].getDay() == 1){
+                            mon++;
+                        }
+                        else if (dates[b].getDay() == 2){
+                            tue++;
+                        }
+                        else if (dates[b].getDay() == 3){
+                            wed++;
+                        }
+                        else if (dates[b].getDay() == 4){
+                            thu++;
+                        }
+                        else if (dates[b].getDay() == 5){
+                            fri++;
+                        }
+                        else if (dates[b].getDay() == 6){
+                            sat++;
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            String mcDayString = " ";
+            maxnumber = Math.max(sat,Math.max(fri,Math.max(wed,Math.max(tue,Math.max(sun,Math.max(thu,mon))))));
+            if (maxnumber == sun) {
+                maxday = 0;
+                mcDayString = "Sunday ";
+            }
+            else if(maxnumber == mon) {
+                    maxday = 1;
+                    mcDayString = "Monday ";
+                }
+            else if(maxnumber == tue) {
+                        maxday = 2;
+                        mcDayString = "Tuesday ";
+                    }
+            else if(maxnumber == wed){
+                mcDayString = "Wednesday ";
+                maxday =3;
+            }
+            else if(maxnumber == thu) {
+                                maxday = 4;
+                mcDayString = "Thursday ";
+                            }
+            else if(maxnumber == fri) {
+                                    maxday = 5;
+                mcDayString = "Friday ";
+                                }
+            else if(maxnumber == sat) {
+                                        maxday = 6;
+                mcDayString = "Saturday ";
+                                    }
+
+
+            int[] returnArray;
+            returnArray = new int[2];
+            returnArray[0]= maxday;
+            returnArray[1] = maxhour;
+
+
+            textView2 = (TextView) findViewById(R.id.textView2);
+            textView2.setText(mcDayString + mcHour);
+
+            System.out.println("Max day is = " + maxday + "Max hour is =" + maxhour);
+            return returnArray;
         }
 
     }
